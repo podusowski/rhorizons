871866b6ac28cc21ce36f07e8f5478ccdd41476e
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-08-13 23:02:25 +0200

    new mod - parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::{num::ParseIntError, str::FromStr};
-
 use serde::Deserialize;
-use thiserror::Error;
+
+use crate::parsing::MajorBody;
+
+mod parsing;
 
 #[derive(Deserialize, Debug)]
 struct HorizonsResponse {
@@ -25,36 +26,5 @@ async fn main() {
     for line in response.result.split("\n") {
         eprintln!("{}", line);
         eprintln!("{:?}", MajorBody::try_from(line));
-    }
-}
-
-/// Planet, natural satellite, spacecraft, Sun, barycenter, or other objects
-/// having pre-computed trajectories.
-///
-/// https://ssd.jpl.nasa.gov/horizons/manual.html#defs
-#[derive(Debug)]
-struct MajorBody {
-    id: i32,
-}
-
-#[derive(Error, Debug)]
-enum MajorBodyParseError {
-    #[error("invalid id")]
-    InvalidId(#[source] ParseIntError),
-}
-
-impl TryFrom<&str> for MajorBody {
-    type Error = MajorBodyParseError;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        Ok(Self {
-            // TODO: Emit some meaningful error.
-            id: value
-                .get(0..9)
-                .unwrap_or("none")
-                .trim()
-                .parse()
-                .map_err(MajorBodyParseError::InvalidId)?,
-        })
     }
 }
---
+++ src/parsing.rs
@@ -0,0 +1,34 @@
+use std::num::ParseIntError;
+
+use thiserror::Error;
+
+/// Planet, natural satellite, spacecraft, Sun, barycenter, or other objects
+/// having pre-computed trajectories.
+///
+/// https://ssd.jpl.nasa.gov/horizons/manual.html#defs
+#[derive(Debug)]
+pub struct MajorBody {
+    id: i32,
+}
+
+#[derive(Error, Debug)]
+pub enum MajorBodyParseError {
+    #[error("invalid id")]
+    InvalidId(#[source] ParseIntError),
+}
+
+impl TryFrom<&str> for MajorBody {
+    type Error = MajorBodyParseError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Ok(Self {
+            // TODO: Emit some meaningful error.
+            id: value
+                .get(0..9)
+                .unwrap_or("none")
+                .trim()
+                .parse()
+                .map_err(MajorBodyParseError::InvalidId)?,
+        })
+    }
+}
