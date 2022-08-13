d6e6f9b1820ac394bad1aec4e71e966d56101e94
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-08-13 22:36:01 +0200

    starting to parse major body

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::str::FromStr;
+
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 struct HorizonsResponse {
-    result: String
+    result: String,
 }
 
 #[tokio::main]
@@ -21,6 +23,26 @@ async fn main() {
 
     for line in response.result.split("\n") {
         eprintln!("{}", line);
-        eprintln!("'{}'", &line.get(0..9).unwrap_or("none"));
+        eprintln!("{:?}", MajorBody::try_from(line));
+    }
+}
+
+/// Planet, natural satellite, spacecraft, Sun, barycenter, or other objects
+/// having pre-computed trajectories.
+///
+/// https://ssd.jpl.nasa.gov/horizons/manual.html#defs
+#[derive(Debug)]
+struct MajorBody {
+    id: i32,
+}
+
+impl TryFrom<&str> for MajorBody {
+    type Error = ();
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        Ok(Self {
+            // TODO: Emit some meaningful error.
+            id: value.get(0..9).unwrap_or("none").parse().map_err(|_| ())?,
+        })
     }
 }
