1922f801b1028b0ab932885173f8d62f2a15db50
Piotr Podusowski <ext-lux-pp@ad.datcon.co.uk>
2022-08-13 22:20:07 +0200

    trying to read major bodies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct HorizonsResponse {
+    result: String
+}
+
+#[tokio::main]
+async fn main() {
+    let response = reqwest::Client::new()
+        .get("https://ssd.jpl.nasa.gov/api/horizons.api")
+        .query(&[("COMMAND", "MB")])
+        .send()
+        .await
+        .unwrap()
+        .json::<HorizonsResponse>()
+        .await
+        .unwrap();
+
+    eprintln!("{:?}", response);
+
+    for line in response.result.split("\n") {
+        eprintln!("{}", line);
+        eprintln!("'{}'", &line.get(0..9).unwrap_or("none"));
+    }
 }
